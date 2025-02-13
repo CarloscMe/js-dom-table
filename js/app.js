168a3824329d7b932e7db2932666b3474ed7913f
carloscMe <carloscarrillo.sprout@gmail.com>
2025-02-13 16:56:07 -0500

    actualizacion

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
-const body = document.getElementsByTagName("body"); // indica el cuerpo de html //
+const body = document.body; // indica el cuerpo de html //
+
+const click = document.querySelector(".click");
+
+click.addEventListener("click", table, { once: true } );
+
+function table (){
 
 let table = document.createElement("table"); // se crea el elemento table //
 
@@ -16,23 +22,17 @@ let thead_item = document.createElement("tr");
 
 thead.appendChild(thead_item);
 
-let item_th1 = document.createElement("th");
+let thead_item_th = ["nombre", "version", "algo asi"];
 
-item_th1.innerText = "informacion";
+thead_item_th.forEach((text) => {
 
-thead_item.appendChild(item_th1);
+    let item_th = document.createElement("th");
 
-let item_th2 = document.createElement("th");
+    item_th.innerText = text;
 
-item_th2.innerText = "version";
-
-thead_item.appendChild(item_th2);
-
-let item_th3 = document.createElement("th");
-
-item_th3.innerText = "algo";
-
-thead_item.appendChild(item_th3);
+    thead_item.appendChild(item_th)
+}
+);
 
 let tbody = document.createElement("tbody");
 
@@ -44,21 +44,85 @@ let tbody_item = document.createElement("tr");
 
 tbody.appendChild(tbody_item);
 
-let item_td1 = document.createElement("td");
+let tbody_item_td1 = ["mario broos", "1.02", "si"];
 
-item_td1.innerText = "objeto";
+tbody_item_td1.forEach((text) =>{
 
-tbody_item.appendChild(item_td1);
+    let item_td1 = document.createElement("td");
 
-let item_td2 = document.createElement("td");
+    item_td1.innerText = text;
 
-item_td2.innerText = "no se";
+    tbody_item.appendChild(item_td1);
+}
+);
 
-tbody_item.appendChild(item_td2);
+let tbody_item2 = document.createElement("tr");
 
-let item_td3 = document.createElement("td");
+tbody.appendChild(tbody_item2);
 
-item_td3.innerText = "imagen";
+let tbody_item_td2 = ["sonic", "3.0", "mas o menos"];
 
-tbody_item.appendChild(item_td3);
+tbody_item_td2.forEach((text) =>{
 
+    let item_td2 = document.createElement("td");
+
+    item_td2.innerText = text;
+
+    tbody_item2.appendChild(item_td2)
+}
+);
+
+let tbody_item3 = document.createElement("tr");
+
+tbody.appendChild(tbody_item3);
+
+let tbody_item_td3 = ["aña", "dd", "ohhyeah"];
+
+tbody_item_td3.forEach((text) =>{
+
+    let item_td3 = document.createElement("td");
+
+    item_td3.innerText = text;
+
+    tbody_item3.appendChild(item_td3)
+}
+);
+
+let tbody_item4 = document.createElement("tr");
+
+tbody.appendChild(tbody_item4);
+
+let tbody_item_td4 = ["nos", "dad", "oahhyeah"];
+
+tbody_item_td4.forEach((text) =>{
+
+    let item_td4 = document.createElement("td");
+
+    item_td4.innerText = text;
+
+    tbody_item4.appendChild(item_td4);
+}
+);
+
+let tfoot = document.createElement("tfoot");
+
+tfoot.classList.add("tfoot");
+
+table.appendChild(tfoot);
+
+let tfoot_item = document.createElement("tr");
+
+tfoot.appendChild(tfoot_item);
+
+let tfoot_item_td = ["resultado", "390.2", "en si"];
+
+tfoot_item_td.forEach((text) =>{
+
+    let item_td5 = document.createElement("td");
+
+    item_td5.innerText = text;
+
+    tfoot_item.appendChild(item_td5);
+}
+);
+};
